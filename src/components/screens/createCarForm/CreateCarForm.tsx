63d519480a966e9726623a2fee5ffb32dec7ee11
Kashubyak Mykola <kashubyak514@gmail.com>
2024-05-08 21:32:12 +0300

    Fixed an error when creating a card, due to which, when entering data, it was immediately displayed `undefined' by the second value in the machine name

--- src/components/screens/createCarForm/CreateCarForm.tsx
+++ src/components/screens/createCarForm/CreateCarForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, SetStateAction, useState } from 'react'
+import { FC, SetStateAction } from 'react'
 import { useForm } from 'react-hook-form'
 import { ICar, ICarData } from 'types/car.interface'
 import { ISideBarState } from 'types/content.interface'
@@ -14,22 +14,15 @@ const CreateCarForm: FC<ISideBarState> = ({ activeMenuItem }) => {
 		register,
 		handleSubmit,
 		formState: { errors },
+		watch,
 	} = useForm<ICarData>({ mode: 'onChange' })
 	const { createCar } = useCreateCar(reset)
 
-	const [car, setCar] = useState<ICar>({} as ICar)
 	const onSubmit = (data: ICarData) => {
-		setCar(data as ICar)
 		createCar(data)
 	}
-	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target
-		setCar(prevState => ({
-			...prevState,
-			[name]: value,
-		}))
-	}
 
+	const car = watch()
 	return (
 		<div className={styles.container}>
 			<Header />
@@ -45,31 +38,27 @@ const CreateCarForm: FC<ISideBarState> = ({ activeMenuItem }) => {
 						<input
 							{...register('name', { required: 'Name is required' })}
 							placeholder='Name car'
-							onChange={handleInputChange}
 						/>
 						{errors?.name?.message && <p style={{ color: '#f00' }}>Name is required</p>}
 						<input
 							{...register('model', { required: 'Model is required' })}
 							placeholder='Model'
-							onChange={handleInputChange}
 						/>
 						{errors?.model?.message && <p style={{ color: '#f00' }}>Model is required</p>}
 						<input
 							{...register('price', { required: 'Price is required' })}
 							placeholder='Price'
-							onChange={handleInputChange}
 						/>
 						{errors?.price?.message && <p style={{ color: '#f00' }}>Price is required</p>}
 						<input
 							{...register('image', { required: 'Image is required' })}
 							placeholder='Image'
-							onChange={handleInputChange}
 						/>
 						{errors?.image?.message && <p style={{ color: '#f00' }}>Image is required</p>}
 						<button className='btn'>Create</button>
 					</form>
 					<div className={styles.card}>
-						<CarItem car={car} />
+						<CarItem car={car as ICar} />
 					</div>
 				</div>
 			</div>
