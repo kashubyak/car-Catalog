2c897f12227d5fb8d99ab4a1af1e269b1e905015
Kashubyak Mykola <kashubyak514@gmail.com>
2024-07-18 15:43:30 +0300

    Work on the search form has been completed - Stretches to the entire available screen width - Added adaptive functionality, convenient and understandable for the user - The maximum height of parameters is limited so that with a large number of parameters it does not occupy the entire screen.

--- src/components/ui/header/searchForm/SearchForm.tsx
+++ src/components/ui/header/searchForm/SearchForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import Select, { ActionMeta, MultiValue } from 'react-select'
 import makeAnimated from 'react-select/animated'
 import { IOption, ISearchFormProps } from 'types/content.interface'
@@ -7,6 +7,9 @@ import { Switch } from './switcher/Switch'
 
 const SearchForm: FC<ISearchFormProps> = ({ onFilter, data }) => {
 	const [selectedOptions, setSelectedOptions] = useState<MultiValue<IOption>>([])
+	const [isSearchActive, setIsSearchActive] = useState(false)
+	const searchContainerRef = useRef<HTMLDivElement>(null)
+
 	const options: IOption[] = data
 		? Array.from(new Set(data.map(item => item.name))).map(name => ({
 				value: name,
@@ -15,6 +18,7 @@ const SearchForm: FC<ISearchFormProps> = ({ onFilter, data }) => {
 		: []
 
 	const animatedComponents = makeAnimated()
+
 	const handleChange = (
 		newValue: MultiValue<IOption>,
 		actionMeta: ActionMeta<IOption>,
@@ -28,16 +32,52 @@ const SearchForm: FC<ISearchFormProps> = ({ onFilter, data }) => {
 		}
 	}
 
+	const toggleSearch = () => {
+		setIsSearchActive(prevState => !prevState)
+	}
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			searchContainerRef.current &&
+			!searchContainerRef.current.contains(event.target as Node) &&
+			!(event.target as HTMLElement).closest('.searchIcon')
+		) {
+			setIsSearchActive(false)
+		}
+	}
+
+	const handleResize = () => {
+		if (window.innerWidth > 600) {
+			setIsSearchActive(false)
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside)
+		window.addEventListener('resize', handleResize)
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [])
+
 	return (
 		<form className='dFlex'>
-			<Select
-				components={animatedComponents}
-				classNamePrefix='custom-styles'
-				onChange={handleChange}
-				options={options}
-				placeholder='Find cars'
-				isMulti
-			/>
+			<div
+				ref={searchContainerRef}
+				className={`searchContainer ${isSearchActive ? 'active' : ''}`}
+			>
+				<Select
+					maxMenuHeight={200}
+					components={animatedComponents}
+					classNamePrefix='custom-styles'
+					onChange={handleChange}
+					options={options}
+					placeholder='Find cars'
+					isMulti
+				/>
+			</div>
+			<i className='fa fa-search searchIcon' aria-hidden='true' onClick={toggleSearch} />
 			<div className='switchTheme'>
 				<Switch />
 			</div>
