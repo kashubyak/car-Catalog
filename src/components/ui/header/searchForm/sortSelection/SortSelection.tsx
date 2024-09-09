a6fc45d196939c24a9ef3dcbd08560e0cfbaf3f9
Kashubyak Mykola <kashubyak514@gmail.com>
2024-09-09 14:19:30 +0300

    The card sorting logic has been moved to a separate hook and optimized for minimal rendering and memory usage.

--- src/components/ui/header/searchForm/sortSelection/SortSelection.tsx
+++ src/components/ui/header/searchForm/sortSelection/SortSelection.tsx
@@ -1,48 +1,12 @@
-import { FC, useEffect, useRef, useState } from 'react'
+import { useSortSelection } from 'hooks/useSortSelection'
+import { FC } from 'react'
 import { IoIosArrowDown } from 'react-icons/io'
 import { ISearchFormProps } from 'types/content.interface'
 import styles from './SortSelection.module.css'
 
 const SortSelection: FC<ISearchFormProps> = ({ onFilter, data }) => {
-	const [isOpen, setIsOpen] = useState(false)
-	const [selectedOptions, setSelectedOptions] = useState('Sort by...')
-	const containerRef = useRef<HTMLDivElement>(null)
-
-	const sortData = (option: string) => {
-		let newSortedData = [...data]
-		if (option === 'By name') {
-			newSortedData.sort((a, b) => a.name.localeCompare(b.name))
-		} else if (option === 'Cheaper at first') {
-			newSortedData.sort((a, b) => {
-				return parseFloat(a.price) - parseFloat(b.price)
-			})
-		} else if (option === 'Expensive at first') {
-			newSortedData.sort((a, b) => {
-				return parseFloat(b.price) - parseFloat(a.price)
-			})
-		}
-		onFilter(newSortedData)
-	}
-
-	const toogleOptions = () => {
-		setIsOpen(!isOpen)
-	}
-	const selectOption = (option: string) => {
-		setSelectedOptions(option)
-		setIsOpen(false)
-		sortData(option)
-	}
-	useEffect(() => {
-		const handleClickOutside = (e: MouseEvent) => {
-			if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
-				setIsOpen(false)
-			}
-		}
-		document.addEventListener('mousedown', handleClickOutside)
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside)
-		}
-	}, [])
+	const { selectedOptions, containerRef, toogleOptions, selectOption, isOpen } =
+		useSortSelection({ onFilter, data })
 
 	return (
 		<div className={styles.selectContainer} ref={containerRef}>
---
+++ src/hooks/useSortSelection.ts
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { ISearchFormProps } from 'types/content.interface'
+
+const useSortSelection = ({ onFilter, data }: ISearchFormProps) => {
+	const [isOpen, setIsOpen] = useState(false)
+	const [selectedOptions, setSelectedOptions] = useState('Sort by...')
+	const containerRef = useRef<HTMLDivElement>(null)
+
+	const sortData = useCallback(
+		(option: string) => {
+			let newSortedData = [...data]
+			if (option === 'By name') {
+				newSortedData.sort((a, b) => a.name.localeCompare(b.name))
+			} else if (option === 'Cheaper at first') {
+				newSortedData.sort((a, b) => {
+					return parseFloat(a.price) - parseFloat(b.price)
+				})
+			} else if (option === 'Expensive at first') {
+				newSortedData.sort((a, b) => {
+					return parseFloat(b.price) - parseFloat(a.price)
+				})
+			}
+			onFilter(newSortedData)
+		},
+		[data, onFilter],
+	)
+	const toogleOptions = useCallback(() => {
+		setIsOpen(prev => !prev)
+	}, [])
+
+	const selectOption = useCallback(
+		(option: string) => {
+			setSelectedOptions(option)
+			setIsOpen(false)
+			sortData(option)
+		},
+		[sortData],
+	)
+	useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
+				setIsOpen(false)
+			}
+		}
+		document.addEventListener('mousedown', handleClickOutside)
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [])
+	return {
+		selectedOptions,
+		containerRef,
+		toogleOptions,
+		selectOption,
+		isOpen,
+	}
+}
+export { useSortSelection }
