71f64ae0d5152e674855ff108f1a8d5bf55ff307
Kashubyak Mykola <kashubyak514@gmail.com>
2024-09-25 18:18:41 +0300

    The services file has been updated, now the site works entirely on github pages

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -2,17 +2,19 @@ import axios from 'axios'
 import { ICar, ICarData } from 'types/car.interface'
 export const CarService = {
 	async getAll() {
-		const response = await axios.get<ICar[]>('http://localhost:4200/cars')
+		const response = await axios.get<ICar[]>('https://json-db-omega.vercel.app/cars')
 		return response.data
 	},
 	async getById(id: string) {
-		const response = await axios.get<ICar[]>(`http://localhost:4200/cars/?id=${id}`)
+		const response = await axios.get<ICar[]>(
+			`https://json-db-omega.vercel.app/cars/?id=${id}`,
+		)
 		return response.data[0]
 	},
 	async create(data: ICarData) {
-		return await axios.post('http://localhost:4200/cars', data)
+		return await axios.post('https://json-db-omega.vercel.app/cars', data)
 	},
 	async delete(id: string) {
-		return await axios.delete(`http://localhost:4200/cars/${id}`)
+		return await axios.delete(`https://json-db-omega.vercel.app/cars/${id}`)
 	},
 }
